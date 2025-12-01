5744f0452c567ffe96cf734dc95ca74c0c63e9aa
billhaha1947 <dangnhatminh18122008@gmail.com>
2025-12-01 20:51:07 +0700

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,14 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  root: './src',  // 👈 báo cho Vite biết src là thư mục gốc frontend
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 5000,
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true
       }
     }
+  },
+  build: {
+    outDir: '../dist',  // 👈 Vercel cần file build nằm trong /frontend/dist
+    emptyOutDir: true
   }
-})
+});
